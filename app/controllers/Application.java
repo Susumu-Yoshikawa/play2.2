eed73d542d6bc772d255d133f3d1d48a183ee5cb
yoshikawasusumu <yoshikawasusumu@192.168.23.156>
2018-06-06 17:58:23 +0900

    1.3.4

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,14 @@
 package controllers;
 
-import play.mvc.Controller;
-import play.mvc.Result;
+import play.*;
+import play.mvc.*;
+
 import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready.!>!>!>!>!>????"));
+        return ok(index.render("これは最初の値です。","これは真ん中の値です。","これは最後のテキストです。"));
     }
 
 }
