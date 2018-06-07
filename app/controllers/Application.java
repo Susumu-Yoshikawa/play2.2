4060c6a966422f1b22095c954e4be328850736ce
yoshikawasusumu <yoshikawasusumu@192.168.23.156>
2018-06-07 16:53:48 +0900

    3.4.4

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,12 +5,15 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import models.Member;
 import models.Message;
 import play.data.Form;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-import scala.Tuple2;
 import views.html.*;
 
 public class Application extends Controller {
@@ -26,6 +29,21 @@ public class Application extends Controller {
 		return ok(index.render("please set form.", msgs));
 	}
 
+	// JSONデータの作成
+	public static Result ajax() {
+		String input = request().body().asFormUrlEncoded().get("input")[0];
+		ObjectNode result = Json.newObject();
+		if(input == null) {
+			result.put("status", "BAD");
+			result.put("message", "Can't get sending data...");
+			return badRequest(result);
+		} else {
+			result.put("status", "OK");
+			result.put("message", input);
+			return ok(result);
+		}
+	}
+
 	// dummy
 	public static Result create() {
 		return redirect("/");
@@ -33,6 +51,7 @@ public class Application extends Controller {
 
 	// 表示用のAction
 	public static Result add() {
+		/*
 		Form<Message> f = new Form(Message.class);
 		List<Member> mems = Member.find.select("name").findList();
 		List<Tuple2<String, String>> opts = new ArrayList<Tuple2<String, String>>();
@@ -40,6 +59,8 @@ public class Application extends Controller {
 			opts.add(new Tuple2(mem.name, mem.name));
 		}
 		return ok(add.render("投稿フォーム", f, opts));
+		*/
+		return redirect("/");
 	}
 
 }
