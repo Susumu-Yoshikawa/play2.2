1704f699f2907b0217a4d2956e749a0d297bbe43
yoshikawasusumu <yoshikawasusumu@192.168.23.156>
2018-06-07 11:41:06 +0900

    2.5.3

--- app/models/Message.java
+++ app/models/Message.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 
 import com.avaje.ebean.annotation.CreatedTimestamp;
 
+import play.data.validation.Constraints.Email;
+import play.data.validation.Constraints.Pattern;
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
@@ -16,12 +18,14 @@ public class Message extends Model {
 	@Id
 	public Long id;
 
-	@Required
+	@Required(message = "必須項目です。")
 	public String name;
 
+	@Email(message = "メールアドレスを記入ください。")
 	public String mail;
 
-	@Required
+	//@MaxLength(200)
+	@Pattern(message = "半角英数字のみにしてください。", value="[a-zA-Z]+")
 	public String message;
 
 	@CreatedTimestamp
